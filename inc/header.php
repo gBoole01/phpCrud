862b9209daa2ae20cca57329d24b49dd4b9faed2
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-03-31 15:47:49 +0200

    style: continue bootstrap integration

--- inc/header.php
+++ inc/header.php
@@ -13,8 +13,23 @@
     <header>
         <nav class="navbar bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">PHP CRUD</a>
-                <button class="btn btn-success" type="button"><a class="link-light text-decoration-none" href="create.php">Ajouter un étudiant</a></button>
+                <a class="navbar-brand d-flex align-items-center gap-2" href="index.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                        <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z" />
+                        <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z" />
+                    </svg>
+                    Students App
+                </a>
+                <button class="btn btn-success" type="button">
+                    <a class="link-light text-decoration-none" href="create.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                            <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                        </svg>
+                        Ajouter un étudiant
+                    </a>
+                </button>
             </div>
         </nav>
     </header>
+    <div class="container-sm">
--- view.php
+++ view.php
@@ -24,26 +24,24 @@ if (isset($_GET['success'])) {
 
 
 include 'inc/header.php' ?>
-<div class="container-sm">
-    <h1>Fiche étudiant</h1>
-    <button class="btn btn-primary mb-3">
-        <a class="link-light text-decoration-none" href="index.php">Retour à l'accueil</a>
-    </button>
-    <div>
-        <?= $editResponseMessage ?>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title"><?= $student['name'] ?></h5>
-            <p class="card-text">Age: <?= $student['age'] ?></p>
-            <p class="card-text">Ecole: <?= $student['school'] ?></p>
-            <button class="btn btn-warning">
-                <a class="link-light text-decoration-none" href="/phpCrud/edit.php?id=<?= $student['id'] ?>">Modifier</a>
-            </button>
-            <button class="btn btn-danger">
-                <a class="link-light text-decoration-none" href="/phpCrud/delete.php?id=<?= $student['id'] ?>">Supprimer</a>
-            </button>
-        </div>
+<h1>Fiche étudiant</h1>
+<button class="btn btn-primary mb-3">
+    <a class="link-light text-decoration-none" href="index.php">Retour à l'accueil</a>
+</button>
+<div>
+    <?= $editResponseMessage ?>
+</div>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title"><?= $student['name'] ?></h5>
+        <p class="card-text">Age: <?= $student['age'] ?></p>
+        <p class="card-text">Ecole: <?= $student['school'] ?></p>
+        <button class="btn btn-warning">
+            <a class="link-light text-decoration-none" href="/phpCrud/edit.php?id=<?= $student['id'] ?>">Modifier</a>
+        </button>
+        <button class="btn btn-danger">
+            <a class="link-light text-decoration-none" href="/phpCrud/delete.php?id=<?= $student['id'] ?>">Supprimer</a>
+        </button>
     </div>
 </div>
 
